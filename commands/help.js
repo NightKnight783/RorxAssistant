bd1958a31647c33330bf6ca0e6d8f4d23ff01485
Loïc Montagna <loic.montagna@polyconseil.fr>
2023-11-23 17:16:27 +0100

    Debut travail sur help.js et memberJoin.js

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, AttachmentBuilder, EmbedBuilder} = require('discord.js');
+const { SlashCommandBuilder, Colors, ActionRowBuilder, StringSelectMenuOptionBuilder, AttachmentBuilder, StringSelectMenuBuilder, EmbedBuilder} = require('discord.js');
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -10,6 +10,73 @@ module.exports = {
 		.setDescription('Donne quelques informations a moitié utiles'),
 	async execute(interaction) {
 
+		const select = new StringSelectMenuBuilder()
+			.setCustomId('help')
+			.setPlaceholder('Choisissez votre sujet.')
+			.addOptions(
+                new StringSelectMenuOptionBuilder()
+                .setLabel('Serveur')
+                .setDescription('Obtenez toutes les informations nécéssaire sur le serveur.')
+                .setValue('server'),
+				new StringSelectMenuOptionBuilder()
+					.setLabel('Commandes')
+					.setDescription('Présentation de la liste des commandes disponibles.')
+					.setValue('command'),
+				new StringSelectMenuOptionBuilder()
+					.setLabel('Système d\'xp')
+					.setDescription('Comprennez le fonctionnement du système d\'experience du serveur.')
+					.setValue('xp')
+			);
+
+        const row = new ActionRowBuilder()
+			.addComponents(select);
+
+        const embedHelp = new EmbedBuilder()
+            .setColor(Colors.Orange)
+            .setAuthor({ name: interaction.user.globalName })
+            .setTitle(`Choisissez le sujet dont vous souhaitez avoir plus d'information`);
+
+
+		const response = await interaction.reply({
+            embeds: [embedHelp],
+			components: [row],
+		});
+
+        try {
+            const confirmation = await response.awaitMessageComponent({ time: 60_000 });
+
+            if (confirmation.values.includes('server')) {
+
+                const embedServer = new EmbedBuilder()
+                    .setColor(Colors.Blue)
+                    .setAuthor({ name: interaction.user.globalName })
+                    .setTitle(`Informations a propos de ${confirmation.values[0]} [NIY]`);
+
+                await confirmation.update({ embeds: [embedServer], components: [] });
+            } else if (confirmation.values.includes('command')) {
+                
+                const embedCommand = new EmbedBuilder()
+                    .setColor(Colors.Blue)
+                    .setAuthor({ name: interaction.user.globalName })
+                    .setTitle(`Informations a propos de ${confirmation.values[0]} [NIY]`);
+
+                await confirmation.update({ embeds: [embedCommand], components: [] });
+            } else if (confirmation.values.includes('xp')) {
+                
+                const embedXp = new EmbedBuilder()
+                .setColor(Colors.Blue)
+                .setAuthor({ name: interaction.user.globalName })
+                .setTitle(`Informations a propos de ${confirmation.values[0]} [NIY]`);
+
+                await confirmation.update({ embeds: [embedXp], components: [] });
+            }
+        } catch(e) {
+            interaction.deleteReply();
+            return;
+        }
+
+
+        /*
         let filename = "Evoli.png";
 
         if ((getRandomInt(100) == 1))
@@ -31,6 +98,6 @@ module.exports = {
             
         interaction.reply(
             { embeds: [embed], files: [file] }
-        );
+        );*/
 	},                
 };
---
+++ events/memberJoin.js
@@ -0,0 +1,22 @@
+const { Client, Events } = require("discord.js")
+
+const channelId = "1162124995451949196"
+
+module.exports = {
+    name: Events.GuildMemberAdd,
+    async execute(member) {
+        
+        const channel = member.client.channels.cache.find(channel => channel.id === channelId)
+        
+        let message = `
+            Bienvenue <@${member.id}>! 
+            - Pour télécharger le jeu c'est ici -> ⁠🎮-play-rorx
+            - Pour t'inscrire au prochain tournoi -> ⁠🏆-tournaments
+            - Pour connaitre les règles du jeu -> ⁠📕-tutorial
+            - Pour toute question -> ⁠❓-questions
+        `
+
+        channel.send(message)
+
+    }
+}
